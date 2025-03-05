eda34a044bd6d17d6adc5c67c02f2665de7bdcf4
Leon-Carlo <carloraul826@gmail.com>
2025-03-05 10:01:36 -0500

    Update personal.sql

--- personal.sql
+++ personal.sql
@@ -6,6 +6,7 @@ CREATE TABLE PERSONAL (
   nombre               VARCHAR(50) NOT NULL,
   apellido paterno              VARCHAR(50) NOT NULL,
   apellido materno            VARCHAR(50) NOT NULL,
+  edad               VARCHAR(2) NOT NULL,
   telefono           VARCHAR(20),
   email              VARCHAR(100),
 );
