9eb7e0f695d254e52bf14960a42b4150770abaa7
Leon-Carlo <carloraul826@gmail.com>
2025-03-05 10:12:05 -0500

    Update practica.sql

--- practica.sql
+++ practica.sql
@@ -4,8 +4,8 @@ USE PERSONALDB;
 DROP TABLE IF EXISTS PERSONALDB;
 CREATE TABLE personalalumno (
   nombre               VARCHAR(50) NOT NULL,
-  apellido_paterno              VARCHAR(50) NOT NULL,
-  apellido_materno            VARCHAR(50) NOT NULL,
+  apellido_paterno   VARCHAR(50) NOT NULL,
+  apellido_materno   VARCHAR(50) NOT NULL,
   edad               VARCHAR(2) NOT NULL,
   telefono           VARCHAR(20),
   email              VARCHAR(100),
