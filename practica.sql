56c2b5edc540c9e372c08101a0750079755d1e7d
Leon-Carlo <carloraul826@gmail.com>
2025-03-05 10:18:34 -0500

    Update practica.sql

--- practica.sql
+++ practica.sql
@@ -2,11 +2,12 @@ CREATE SCHEMA IF NOT EXISTS PersonalDB;
 USE PersonalDB;
 
 DROP TABLE IF EXISTS personal;
+
 CREATE TABLE Personal(
-  nombre               VARCHAR(50) NOT NULL,
-  apellido_paterno   VARCHAR(50) NOT NULL,
-  apellido_materno   VARCHAR(50) NOT NULL,
-  edad               VARCHAR(2) NOT NULL,
-  telefono           VARCHAR(20),
-  email              VARCHAR(100),
+  nombre              VARCHAR(50) NOT NULL,
+  apellido_paterno    VARCHAR(50) NOT NULL,
+  apellido_materno    VARCHAR(50) NOT NULL,
+  edad                VARCHAR(2) NOT NULL,
+  telefono            VARCHAR(20),
+  email               VARCHAR(100)
 );
